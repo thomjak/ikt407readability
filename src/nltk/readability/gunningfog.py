c1881f13f04c4887a5a6413dcbc192c78914a59c
thomas.skardal <thomas.skardal@0be5db9c-f73b-0410-b83b-bba42594185c>
2007-10-08 10:13:51 +0000

    Small update

---
+++ src/nltk/readability/gunningfog.py
@@ -0,0 +1,24 @@
+import nltk
+import nltk.tokenize
+
+class gunninfog:
+    
+    __text = ""
+    
+    def __init__(self, text=""):
+        self.__text = text
+        
+    def setText(self,text):
+        self.__text = text
+        
+    def calc(self):
+        if len(self.text) == 0:
+            print 'No text set'
+            
+        words = []
+        sentences = []
+        complex_words = []
+        
+        
+    
+    
