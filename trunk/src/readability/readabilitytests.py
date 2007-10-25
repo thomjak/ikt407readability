72ccc1202936bcb7e5811bddf6c8e94e4a4c5032
ezombort <ezombort@0be5db9c-f73b-0410-b83b-bba42594185c>
2007-10-25 17:13:24 +0000

    Added Flesch-Kincaid Grade Level

--- trunk/src/readability/readabilitytests.py
+++ trunk/src/readability/readabilitytests.py
@@ -32,3 +32,19 @@ def FleschReadingEase(text=''):
     
     score = 206.835 - 1.015 * (totalWords / totalSentences) - 84.6 * (totalSyllables / totalWords)
     return score
+
+def FleschKincaidGradeLevel(text=''):
+    score = 0.0
+    words = textanalyzer.getWords(text)
+    sentences = textanalyzer.getSentences(text)
+    
+    totalWords = float(len(words))
+    totalSentences = float(len(sentences))
+    totalSyllables = float(textanalyzer.countSyllables(words))
+    
+    score = 0.39 * (totalWords / totalSentences) + 11.8 * (totalSyllables/totalWords) - 15.59
+    return score
+
+    
+    
+    
