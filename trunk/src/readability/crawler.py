0cd148786daefb0a04d3c8c95dddd1a6da27d973
thomas.skardal <thomas.skardal@0be5db9c-f73b-0410-b83b-bba42594185c>
2007-11-04 14:40:05 +0000

    Added urlextracter.py - Extracts text from a url Added crawler.py - uses urlextracter to extract the text from one of the random links from current url Added languageclassifier.py - naive bayes language classifier

---
+++ trunk/src/readability/crawler.py
@@ -0,0 +1,88 @@
+import random
+from urlextracter import *
+from sgmllib import *
+
+class Crawler:
+    
+    urls = []
+    crawled = []
+    
+    def crawl(self,url):
+        print "Crawling " + url
+        try:
+            ue = URLextracter(url)
+        except SGMLParseError:
+            print "This URL contains error that can't be handled by this app.\nSorry!"
+            print "=" * 30
+            print "Trying new random URL"
+            self.crawl(self.urls[random.randint(1,len(self.urls))])
+            return
+        
+        urlparts = url.replace("http://", "").replace("/",".").split(".")
+        filename = ""
+        for part in urlparts:
+            if not str(part).__contains__("www"):
+                filename += part + "."
+        filename += "txt"
+        
+        print "Stored as: " + filename
+        urls = ""
+        try:
+            f = open("/home/thomas/mined/eng/%s" % filename, 'wb')
+            content = str(ue.output())
+            lines = content.replace("\t", " ").replace("\r"," ").splitlines()
+            content = []
+            for line in lines:
+                temp = line.strip()
+                if temp == "" or temp == " ":
+                    lines.remove(line)
+                else:
+                    if len(line.strip().split(" ")) > 2:
+                        content.append(line.strip() + "\n")
+            f.write("".join(content))
+            f.close()
+            urls = ue.linklist
+            print "" + url + " mined!"
+        except IOError:
+            print "Mined, but failed to store as file.\nSkipping this, going on to next!"
+            urls = self.urls
+        ok_urls = []
+        for i in urls:
+            url = i
+            if str(url).__contains__("javascript"):
+                urls.remove(url)
+            elif str(url).__contains__("http://"):
+                ok_urls.append(url)
+            elif str(url).__contains__("https://"):
+                ok_urls.append(url)
+            else:
+                urls.remove(url)
+        
+        if len(ok_urls) < 2:
+            ok_urls = self.crawled
+        
+        next = random.randint(1,len(ok_urls))
+        unique = False
+        while not unique:
+            new_url = ok_urls[next]
+            if not new_url in self.crawled:
+                unique = True
+            elif len(ok_urls) < 2:                    # with this few left we fake it
+                ok_urls = self.crawled
+                next = random.randint(1,len(ok_urls))
+                new_url = ok_urls[next]
+                unique = True
+            else:
+                print "Already crawled " + new_url
+                ok_urls.remove(new_url)
+                
+        self.urls = ok_urls
+        self.crawled.append(url)
+        self.crawl(new_url)
+
+
+
+if __name__=="__main__":
+    url = "http://www.woman24.no/"
+    c = Crawler()
+    c.crawl(url)
